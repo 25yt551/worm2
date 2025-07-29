ab3a2bc42eaca2e22fe074d47d3dcba5611ca37c
25yt551 <amirrchr@gmail.com>
2025-07-29 22:54:07 +0200

    Create uses.php

---
+++ api/uses.php
@@ -0,0 +1,19 @@
+{
+  "id": 1,
+  "cliente_NOMBRE": "Tnen_up",
+  "cliente_ID": "gg_101324750622109474079",
+  "Client_KeyAccecs": "XTPRIVATESKIN",
+  "cliente_DateExpired": "2029-02-14",
+  "status": 1,
+  "Client_VisibleSkin": "1401",
+  "Client_VisibleSkin1": "0000",
+  "Client_VisibleSkin2": "0000",
+  "Client_VisibleSkin3": "0000",
+  "Client_VisibleSkin4": "0000",
+  "Client_VisibleSkin5": "0000",
+  "Client_VisibleSkin6": "0000",
+  "Client_VisibleSkin7": "0000",
+  "Client_VisibleSkin8": "0000",
+  "Client_VisibleSkin9": "0000",
+  "Client_VisibleSkin10": "0000"
+}
