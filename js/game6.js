c8fe36e95b577f46a764f12c15aa16387259b4f4
25yt551 <amirrchr@gmail.com>
2025-08-10 17:06:38 +0200

    Update game6.js

--- js/game6.js
+++ js/game6.js
@@ -341,7 +341,7 @@ async function fetchServersWithRetry(p16, p17 = 3, p18 = 2000) {
 async function loadServers() {
   try {
     const v18 = await fetchServersWithRetry(
-      "https://zwormextenstion.com/wormExtension/api/serversTimMap.php"
+      "https://zwormextenstion.com/wormExtension/api/servers/wormworld.php"
     );
     if (v18.success) {
       let v19 = v18.servers;
