8736e856fc40ca11475061199ec274098ae6cfb0
25yt551 <amirrchr@gmail.com>
2025-08-14 18:37:23 +0200

    Update game8.js

--- js/game8.js
+++ js/game8.js
@@ -420,7 +420,7 @@ const ctx = {
       stroke: "#ff2222",
       strokeThickness: 1,
       whiteSpace: "normal",
-      fontFamily: "cursive",
+      fontFamily: "normal",
       fontWeight: "bold",
       wordWrap: true,
     }),
